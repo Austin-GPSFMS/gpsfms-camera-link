115ef8b2a15905e69e1427e649bed2925c694465
Austin-GPSFMS <austin@gpsfms.com>
2025-04-30 16:04:37 -0400

    Create config.js

---
+++ app/config.js
@@ -0,0 +1,16 @@
+{
+  "name": "Camera Solutions Button",
+  "supportEmail": "support@gpsfms.com",
+  "version": "1.0.0",
+  "items": [
+    {
+      "page": "Inventory",
+      "click": "app/cameraButton.js",
+      "buttonName": {
+        "en": "Open Camera Solutions"
+      },
+      "icon": "https://cdn.jsdelivr.net/gh/Geotab/sdk-addin-samples@master/storage-api-sample/app/images/icon.svg"
+    }
+  ],
+  "isSigned": false
+}
