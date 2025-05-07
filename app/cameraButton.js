119b386c719202ec45addeedde9cf98724e6b4fe
Austin-GPSFMS <austin@gpsfms.com>
2025-05-07 07:30:59 -0400

    Update cameraButton.js

--- app/cameraButton.js
+++ app/cameraButton.js
@@ -3,5 +3,5 @@
 geotab.customButtons = geotab.customButtons || {};
 geotab.customButtons.cameraSolutionsButton = function(event, api, state) {
   event.preventDefault();
-  window.open('https://www.gpsfms.com/geotab-camera-solutions', '_blank');
+  window.open('https://info.gpsfms.com/gpsfms-ai-safety-cameras', '_blank');
 };
